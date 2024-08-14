a9500a70c324db8341c57bd14d3344b47c23ffe6
blacktop <blacktop@users.noreply.github.com>
2024-08-14 13:11:24 -0600

    chore: prepare to support NEW IDA Pro 9.0 plugin JSON

--- plugins/ida/install.sh
+++ plugins/ida/install.sh
@@ -8,8 +8,10 @@ fi
 
 CWD="$(cd -P -- "$(dirname -- "$0")" && pwd -P)"
 
+: ${IDA_PLUGIN_JSON:=$CWD/ida-plugin.json}
 : ${IDA_PLUGIN_FILE:=$CWD/symbolicate.py}
 : ${IDA_PLUGIN_FOLDER:=$HOME/.idapro/plugins/}
+# : ${IDA_PLUGIN_FOLDER:=$HOME/.idapro/plugins/symbolicate}
 
 if [[ "${1-}" =~ ^-*h(elp)?$ ]]; then
     echo 'Usage: install.sh IDA_PLUGIN
@@ -22,7 +24,11 @@ fi
 
 main() {
     echo "  🚀 Installing $IDA_PLUGIN_FILE to $IDA_PLUGIN_FOLDER"
-    cp $IDA_PLUGIN_FILE $IDA_PLUGIN_FOLDER
+    # TODO: change to the NEW way once IDA Pro 9.0 comes out
+    # mkdir -p "$IDA_PLUGIN_FOLDER"
+    # cp "$IDA_PLUGIN_JSON" "$IDA_PLUGIN_FOLDER"
+    # cp "$IDA_PLUGIN_FILE" "$IDA_PLUGIN_FOLDER"
+    cp "$IDA_PLUGIN_FILE" "$IDA_PLUGIN_FOLDER"
     echo "  🎉 Done!"
 }
 
