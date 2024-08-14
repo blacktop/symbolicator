d2fc7dd97ecb9e89a689254f46b8a83018f79bcb
blacktop <blacktop@users.noreply.github.com>
2024-08-14 16:51:33 -0600

    chore: use custom logger in binja plugin

--- plugins/binja/__init__.py
+++ plugins/binja/__init__.py
@@ -22,53 +22,59 @@
 
 import json
 from binaryninja import *
+from binaryninja import BinaryView, Logger
 
 
 def run(bv):
+    log = bv.create_logger("Plugin.Symbolicate")
     json_file_path = get_open_filename_input("Select JSON file")
     if not json_file_path:
-        log_info("No file selected. Exiting.")
+        log.log_info("No file selected. Exiting.")
         return
 
     try:
         with open(json_file_path, "r") as f:
-            symbols_json = json.load(f)
+            symbols = json.load(f)
     except Exception as e:
-        log_error(f"Error reading JSON file: {str(e)}")
+        log.log_error(f"Error reading JSON file: {str(e)}")
         return
+    apply_symbols_and_create_functions(bv=bv, log=log, symbols=symbols)
 
-    apply_symbols_and_create_functions(bv, symbols_json)
 
-
-def apply_symbols_and_create_functions(bv, symbols_json):
+def apply_symbols_and_create_functions(bv: BinaryView, log: Logger, symbols: dict):
     count = 0
-    for address_str, symbol_name in symbols_json.items():
+    for address_str, symbol_name in symbols.items():
         try:
             # Convert address string to int
             address = int(address_str, 10)
             # Ensure the address is valid
             if address < bv.start or address >= bv.end:
-                log_error(f"Address {hex(address)} is out of range for this binary")
+                log.log_info(f"Address {hex(address)} is out of range for this binary")
                 continue
             # Create a function if it doesn't exist
             if not bv.get_function_at(address):
                 bv.create_user_function(address)
-                log_info(f"Created function at address {hex(address)}")
+                log.log_info(f"Created function at address {hex(address)}")
             # Get the function
             func = bv.get_function_at(address)
             if func:
                 # Set the function name (which also creates the symbol)
                 func.name = symbol_name
-                # log_info(f"Applied symbol and set function name: {symbol_name} at address {hex(address)}")
-                print(f"[Symbolicated] 0x{hex(address)}: {symbol_name}")
+                # log.log_info(f"Applied symbol and set function name: {symbol_name} at address {hex(address)}")
+                log.log_debug(f"[Symbolicated] 0x{hex(address)}: {symbol_name}")
             else:
-                log_error(f"Failed to create or get function at address {hex(address)}")
+                log.log_error(f"Failed to create or get function at address {hex(address)}")
             count += 1
         except ValueError:
-            log_error(f"Error parsing address: {address_str}")
+            log.log_error(f"Error parsing address: {address_str}")
         except Exception as e:
-            log_error(f"Error processing symbol: {symbol_name} at {address_str} - {str(e)}")
-    print(f"🎉 Symbolicated {count} addresses 🎉")
+            log.log_error(f"Error processing symbol: {symbol_name} at {address_str} - {str(e)}")
+    log.log_info(f"🎉 Symbolicated {count} addresses 🎉")
 
 
-PluginCommand.register("`ipsw` Symbolicate Plugin", "Symbolicate kernelcache with symbols.json", run)
+def is_valid(bv: BinaryView) -> bool:
+    bv.create_logger("Plugin.Symbolicate")
+    return True
+
+
+PluginCommand.register("`ipsw` Symbolicate Plugin", "Symbolicate kernelcache with symbols.json", run, is_valid)
